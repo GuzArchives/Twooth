5b34a122c682e3b773473ce18cf2639147c7702a
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-05-30 10:24:49 -0300

    chore: bump version

--- src/lib/state/config.ts
+++ src/lib/state/config.ts
@@ -18,6 +18,7 @@ export const version = persistent<string>('twooth.version', '1.0.0');
 export function updateAppState(): void {
 	if (get(version) !== PUBLIC_DEPLOYED_VERSION) {
 		console.log('Updated!');
+		version.set(PUBLIC_DEPLOYED_VERSION);
 	}
 	return;
 }
